fcd99c2a2ebcdbf09915a6e511954922b077f515
zicong <zicong@igola.com>
2017-01-15 19:28:31 +0800

    planBoard complete

--- planBoard/src/main.js
+++ planBoard/src/main.js
@@ -1,11 +1,47 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<App/>',
-  components: { App }
+import App from './App'
+import Home from './components/Home.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import store from './store'
+import TimeEntries from './components/TimeEntries.vue'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+const routes = [
+  {
+    path: '/',
+    component: Home
+  },
+  {
+    path: '/home',
+    component: Home
+  },
+  {
+    path: '/time-entries',
+    component: TimeEntries,
+    children: [
+      {
+        path: 'log-time',
+        component: resolve => require(['./components/LogTime.vue'],resolve)
+      }
+    ]
+  }
+]
+
+const router = new VueRouter({
+  routes: routes
 })
+
+
+var app = new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App),
+});
